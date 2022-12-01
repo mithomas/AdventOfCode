482ea7fe6e6b4075c1441a56bb8e4a2e911e07c1
Michael Thomas <mth@michael-thomas.net>
2022-12-01 22:47:52 +0100

    Completed day 1 of 2022.

---
+++ src/main/kotlin/de/mthix/adventofcode/year2022/Day1.kt
@@ -0,0 +1,17 @@
+package de.mthix.adventofcode.year2022
+
+import de.mthix.adventofcode.linesOfDay
+
+fun main() {
+    val linesOfDay = linesOfDay(2022, 1)
+        .joinToString(" ")
+        .split("  ")
+        .map { it.split(" ") }
+        .map { it.sumBy { it.toInt() }}
+        .sortedBy { it }
+        .reversed()
+
+    println(linesOfDay)
+    println("Puzzle 1: " + linesOfDay.max())
+    println("Puzzle 2: " + (linesOfDay[0] + linesOfDay[1] + linesOfDay[2]))
+}
