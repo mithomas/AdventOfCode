0664a74f9f458f180f91238033c7e86e77e9cbf3
Michael Thomas <mth@michael-thomas.net>
2022-12-03 12:08:43 +0100

    Completed day 3 of 2022.

---
+++ src/main/kotlin/de/mthix/adventofcode/year2022/Day3.kt
@@ -0,0 +1,25 @@
+package de.mthix.adventofcode.year2022
+
+import de.mthix.adventofcode.linesOfDay
+
+fun main() {
+    val linesOfDay = linesOfDay(2022, 3)
+        .map { it.toCharArray().asList() }
+
+    val elements = linesOfDay
+        .map { listOf(it.subList(0, it.size/2), it.subList(it.size/2, it.size)) }
+        .map { it[0].intersect(it[1]).elementAt(0) }
+        .map { calcPriority(it) }
+
+    println(elements)
+    println("Puzzle 1: " + elements.sum())
+
+    val badges = linesOfDay.chunked(3)
+        .map { it[0].intersect(it[1]).intersect(it[2]).elementAt(0) }
+        .map { calcPriority(it) }
+
+    println(badges)
+    println("Puzzle 2: " + badges.sum())
+}
+
+fun calcPriority(c: Char) = if(c.isLowerCase()) { c - 'a' + 1; } else { c - 'A' + 27; }
