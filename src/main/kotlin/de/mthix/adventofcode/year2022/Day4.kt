27bdfecaafddfc71161ec49353a485be3b3a6d39
Michael Thomas <mth@michael-thomas.net>
2022-12-15 15:34:32 +0100

    Improved day 4 of 2022.

--- src/main/kotlin/de/mthix/adventofcode/year2022/Day4.kt
+++ src/main/kotlin/de/mthix/adventofcode/year2022/Day4.kt
@@ -5,13 +5,13 @@ import de.mthix.adventofcode.linesOfDay
 fun main() {
     val linesOfDay = linesOfDay(2022, 4)
         .map { line -> line.split(",") }
-        .map { line -> line
-            .map { assignment -> assignment.split("-") }
-            .map { assignment -> (assignment[0].toInt()..assignment[1].toInt()).toList() }
+        .map { line ->
+            line
+                .map { assignment -> assignment.split("-") }
+                .map { assignment -> (assignment[0].toInt()..assignment[1].toInt()).toList() }
         }
 
     println(linesOfDay)
-    println("Puzzle 1: " + linesOfDay.filter { it[0].
-    containsAll(it[1]) || it[1].containsAll(it[0]) }.size)
-    println("Puzzle 2: " + linesOfDay.filter { it[0].union(it[1]).size < (it[0].size + it[1].size)}.size)
+    println("Puzzle 1: " + linesOfDay.filter { it[0].containsAll(it[1]) || it[1].containsAll(it[0]) }.size)
+    println("Puzzle 2: " + linesOfDay.filter { it[0].union(it[1]).size < (it[0].size + it[1].size) }.size)
 }
